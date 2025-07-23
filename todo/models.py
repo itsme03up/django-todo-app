cd5a105674292c1cf8538b8c8fcca97bf5fe68bb
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-24 00:12:16 +0900

    Add null=True to user foreign key in Task model for optional user association

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
