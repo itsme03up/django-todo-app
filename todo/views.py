fd52a8ac6d8a08136adde195f9ff06f1d37bb40c
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-23 23:57:12 +0900

    Refactor toggle_complete view to handle task completion status

--- todo/views.py
+++ todo/views.py
@@ -18,3 +18,9 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect('/')
+
+def toggle_complete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('/')
