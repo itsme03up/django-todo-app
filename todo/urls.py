5046a2220dd1446a5a422fb3621394813daca306
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-23 23:58:18 +0900

    Add URL pattern for toggling task completion status

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('toggle/<int:task_id>/', views.toggle_complete, name='toggle_complete'),
+
 ]
