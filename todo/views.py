39028c969565268235d9d863d96261a4edc5ef16
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-23 23:33:06 +0900

    Implement task listing and creation in index view

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, TODO!")
+    tasks = Task.objects.all()
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('/')
+
+    return render(request, 'todo/index.html', {'tasks': tasks})
