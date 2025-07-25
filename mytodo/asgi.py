a257991c5668b7fa5b8560fd26474a46f975bd58
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-25 16:55:46 +0900

    Add chat application with WebSocket support for real-time messaging

--- mytodo/asgi.py
+++ mytodo/asgi.py
@@ -8,21 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # we'll make this later
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytodo.settings')
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mytodo.settings")
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Uncomment the following lines to enable WebSocket support:
-    # "websocket": URLRouter([
-    #     # Import your WebSocket URL patterns here
-    #     *todo.routing.websocket_urlpatterns,
-    # ]),
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
+# Add WebSocket support via Channels with chat routing
