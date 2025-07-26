c0d92875a3d1eac27cfd317aa0b2b83f7cdf58ec
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-26 20:22:58 +0900

    Add CORS support by including django-cors-headers in settings and requirements

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     "channels",
     "todo",
     "chat",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -93,6 +95,14 @@ DATABASES = {
         conn_max_age=600
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+if 'RENDER_EXTERNAL_URL' in os.environ:
+    CORS_ALLOWED_ORIGINS.append(os.environ.get('RENDER_EXTERNAL_URL'))
 
 
 # Password validation
