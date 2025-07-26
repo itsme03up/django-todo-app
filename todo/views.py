bd669783033cccdbfddff8ed124f4c875fbbcfcb
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-07-26 20:22:03 +0900

    Refactor Task model and views to enforce user association for tasks

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect,  get_object_or_404
 from .models import Task
+from django.contrib.auth.decorators import login_required
 from django.db import connection
 from django.http import HttpResponse
 import traceback
@@ -8,6 +9,7 @@ from django.conf import settings
 
 # Create your views here.
 
+@login_required
 def index(request):
     try:
         # Detailed debugging information
@@ -24,14 +26,14 @@ def index(request):
             debug_info.append("ERROR: todo_task table not found!")
             return HttpResponse("<br>".join(debug_info), status=500)
         
-        # Try to get tasks
-        tasks = Task.objects.all()
+        # Try to get tasks for the logged-in user
+        tasks = Task.objects.filter(user=request.user)
         debug_info.append(f"Number of tasks found: {tasks.count()}")
 
         if request.method == 'POST':
             title = request.POST.get('title')
             if title:
-                Task.objects.create(title=title)
+                Task.objects.create(title=title, user=request.user)
             return redirect('/')
             
         return render(request, 'todo/index.html', {'tasks': tasks})
@@ -45,13 +47,15 @@ def index(request):
         ]
         return HttpResponse("<pre>" + "\n".join(error_info) + "</pre>", status=500)
 
+@login_required
 def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
     task.delete()
     return redirect('/')
 
+@login_required
 def toggle_complete(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
     task.completed = not task.completed
     task.save()
     return redirect('/')
